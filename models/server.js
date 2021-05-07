ebf02cbb1a3b7ad5733a0a8e668f5c1b1b2d5c50
ramirez-sebas1010 <ramirezmatias946@gmail.com>
2021-05-07 10:57:12 -0400

    Agregamos el Server con las rutas, middlewares, etc.

---
+++ models/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import http from "http";
+import https from "https";
+import cors from "cors";
+
+export default class Server {
+  constructor() {
+    this.portHttp = process.env.PORT_DESAROLLO;
+    this.portHttps = process.env.PORT_PROD;
+    this.app = express();
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.app.use(express.json({ extended: false }));
+    this.app.use(express.static("public"));
+    this.app.use(cors());
+  }
+
+  routes() {
+    this.app.get("/prueba", (req, res, next) => {
+      return res.status(200).json("hola");
+    });
+  }
+
+  createHttpServer() {
+    let httpServer = http.createServer(this.app);
+    httpServer.listen(this.portHttp, () => {
+      console.log(`Corriendo http://localhost:${this.portHttp}`);
+    });
+  }
+
+  createHttpsServer() {
+    //Agregar Clave de Seguridad para https
+    let privateKey = fs.readFileSync("ruta_clave", "utf8");
+    let certificate = fs.readFileSync("ruta_certificado", "utf8");
+
+    let creedenciales = { key: privateKey, cert: certificate };
+    let httpsServer = https.createServer(creedenciales);
+    httpsServer.listen(this.portHttps, () => {
+      console.log("SERVER");
+    });
+  }
+}
